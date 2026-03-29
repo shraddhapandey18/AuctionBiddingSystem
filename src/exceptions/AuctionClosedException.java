6eb773c8d4e970d16cd6faf436771db2946526c2
shraddhapandey18 <k22shraddhapandey@gmail.com>
2026-03-29 19:06:12 +0530

    Initial project structure and exceptions

--- src/exceptions/AuctionClosedException.java
+++ src/exceptions/AuctionClosedException.java
@@ -1,25 +1,5 @@
-package exceptions;
-
-/**
- * Exception thrown when attempting to bid on a closed auction.
- * This is an unchecked exception (extends RuntimeException).
- */
-public class AuctionClosedException extends RuntimeException {
-
-    /**
-     * Constructor with message
-     * @param message Description of the error
-     */
+public class AuctionClosedException extends Exception {
     public AuctionClosedException(String message) {
         super(message);
-    }
-
-    /**
-     * Constructor with message and cause
-     * @param message Description of the error
-     * @param cause The underlying cause
-     */
-    public AuctionClosedException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
